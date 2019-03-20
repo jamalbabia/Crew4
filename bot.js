d98bb2d4a187f469f95388df5dbba6000a8ecc73
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2019-03-20 03:02:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -274,6 +274,17 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    });
  
  
- 
+ client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('بتاتس')){
+if (message.author.id !== '474354424391663616') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
  
 client.login(process.env.BOT_TOKEN);
