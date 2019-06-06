05abff7c33d216a13482bdcbd783fe7185e58ff5
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2019-06-06 10:55:03 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("bhelp | بطاطا",{type: 'WATCHING'})
+   client.user.setActivity("bhelp | بطاطس",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
